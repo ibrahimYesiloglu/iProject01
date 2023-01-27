a8c469be345bab85aae630b62bb9a25c212bdbc5
ibrahimYesiloglu <cizgi.2d@gmail.com>
2023-01-27 16:21:59 +0100

    erste aufgabe.schreibt "hallo" wwird...

---
+++ src/Student/Schule.java
@@ -0,0 +1,10 @@
+package Student;
+
+public class Schule {
+    public static void main(String[] args) {
+
+        System.out.println("Hallo Erde " +
+                "was gehts " +
+                ":)");
+    }
+}
